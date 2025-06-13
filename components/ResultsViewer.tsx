4410395e437ea6d48c8199469fa4adcb8a673a71
ssr-inet <sathyaswaruban@inetcsc.com>
2025-06-13 15:27:03 +0530

    First live build

--- components/ResultsViewer.tsx
+++ components/ResultsViewer.tsx
@@ -50,7 +50,8 @@ export const ResultsViewer = memo(({ responseData }: ResultsViewerProps) => {
         { key: "IHUB_INT_VEND_SUC", label: "Vendor_Suc - Ihub_Ini" },
         { key: "VEND_FAIL_IHUB_INT", label: "Vendor_Fail - Ihub_Ini" },
         { key: "Tenant_db_ini_not_in_hubdb", label: "TENANT_DB_INI_NOT_IN_HUB" },
-        { key: "matched", label: "Matched_Values" }
+        { key: "matched", label: "Matched_Values" },
+        { key: "not_in_vendor", label: "Not_In_Vendor" },
     ];
 
     const activeSections = dataSections.filter(section => {
